551b96f8dd8e5380d274801362b48798e918e0aa
Joshua Rowe <joshua@wired.com.au>
2012-06-01 19:30:00 +1000

    Testing Aptana Studio

--- Game/Game.rb
+++ Game/Game.rb
@@ -12,6 +12,8 @@ $file = File.new("log.txt", "w+")
 
 start				# Runs start method which intialises everything needed including Field Controllers, get player's name, graphics etc
 
+# Gotta make this loop 2-player friendly at somepoint
+
 while $lp1.returnlp > 0		# If life points become 0 the game loop will close.
 	activephase($activeplayer)
   	if $lp1.returnlp <= 0		# Lose condition: If your lifepoints reach 0
